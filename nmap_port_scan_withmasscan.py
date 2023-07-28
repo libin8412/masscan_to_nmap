994d1731aa9199601f2343611fa1294c0f329fb7
libin8412 <libinlarry@163.com>
2023-07-28 16:57:19 +0800

    Update nmap_port_scan_withmasscan.py

--- nmap_port_scan_withmasscan.py
+++ nmap_port_scan_withmasscan.py
@@ -8,24 +8,19 @@ import time
 import multiprocessing
 import os
 import json
-
-# 全局扫描参数
-# global_options = '-sT -P0 -sV -O --script=banner -p T:21-25,80-89,110,143,443,513,873,1080,1433,1521,1158,3306-3308,3389,3690,5900,6379,7001,8000-8090,9000,9418,27017-27019,50060,111,11211,2049'
-# global_options ='-sT -sV -p 445 -script-args http.useragent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/537.36"'
-# global_options ='-sT -sV -p 1-65535 -script-args http.useragent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/537.36"'
+import requests
+import chardet
+import re
 
 # 处理端口状态
 global_port_states =['open']
-
 # 使用masscan扫描,存储扫描结果，以作为nmap扫描的输入源
 if os.path.exists('test_json4.json'):
     os.remove('test_json4.json')
-os.system('masscan -iL scan_ip2.txt -p1-65535 -oJ test_json4.json --rate 10000')
+os.system('/Users/larry/Program/masscan/bin/masscan -iL scan_ip.txt -p1-65535 -oJ test_json4.json --rate 50000')
 # os.system('masscan -iL scan_ip4.txt -pU:1-65535 -oJ test_json4_udp.json --rate 10000') udp扫描
 
 # 定义函数，用于进行扫描
-
-
 def do_nmap_scan(scan_ip_list, scan_port):
     # nmap自定义UA，避免被WAF检测到
     nmap_proc = NmapProcess(scan_ip_list, options='-sT -sV -p ' + str(scan_port) + ' -script-args http.useragent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/537.36"')
@@ -34,8 +29,40 @@ def do_nmap_scan(scan_ip_list, scan_port):
     for host in nmap_repot.hosts:
         for serv in host.services:
             if serv.state in global_port_states:
-                print 'scan_host is %s,scan result is %s|%s|%s|%s|%s'\
-                      % (host.address, str(serv.port), serv.protocol, serv.state, serv.service, serv.banner)
+                if serv.service == "http":
+                    scan_url = 'http://' + host.address + ':' + str(serv.port)
+                    Title(scan_url)
+                else:
+                    print('scan_host is %s,scan result is %s|%s|%s|%s|%s' \
+                          % (host.address, str(serv.port), serv.protocol, serv.state, serv.service, serv.banner))
+
+
+
+# 获取网站标题
+def Title(scan_url_port):
+    try:
+        r = requests.get(scan_url_port, timeout=5, verify=False, stream=True)
+        # 获取网站的页面编码
+        if 'Content-Length' in r.headers.keys() and int(r.headers['Content-Length']) > 50000:  # 有些人特别坏访问端口让你下载个几g的文件
+            print('[*]主机 ' + scan_url_port + ' 端口服务为：' + '大文件')
+        else:
+            r_detectencode = chardet.detect(r.content)
+            actual_encode = r_detectencode['encoding']
+            response = re.findall(u'<title>(.*?)</title>', r.content.decode(actual_encode), re.S)
+            if response == []:
+                pass
+            else:
+                # 将页面解码为utf-8，获取中文标题
+                res = response[0]
+                banner = r.headers['server']
+                print(
+                    scan_url_port + '\t' + "".join(banner.split()) + '\t' + ''.join(res.split()) + '\t' + str(
+                        r.status_code) + '\t' + str(len(r.content)))
+
+    except Exception as e:
+        pass
+
+
 # 定义扫描开始时间
 time_start = time.time()
 
@@ -48,13 +75,10 @@ if __name__ == "__main__":
         str1 = file.read()
         str2 = str1[:-4] + str1[-3:]
         data = json.loads(str2)
-        for i in xrange(len(data)):
+        for i in range(len(data)):
             scan_ip_list = data[i]['ip']
             scan_port = data[i]['ports'][0]['port']
             pool.apply_async(do_nmap_scan, args=(str(scan_ip_list), scan_port,))
     pool.close()
     pool.join()
-    print '扫描时间为%s秒' % (time.time() - time_start)
-
-
-
+    print('扫描时间为%s秒' % (time.time() - time_start))
