02893d27cc93228b81abfc0d45eb19521304b809
libin8412 <libinlarry@163.com>
2023-07-28 17:04:20 +0800

    Update nmap_port_scan_withmasscan.py

--- nmap_port_scan_withmasscan.py
+++ nmap_port_scan_withmasscan.py
@@ -43,7 +43,7 @@ def Title(scan_url_port):
     try:
         r = requests.get(scan_url_port, timeout=5, verify=False, stream=True)
         # 获取网站的页面编码
-        if 'Content-Length' in r.headers.keys() and int(r.headers['Content-Length']) > 50000:  # 有些人特别坏访问端口让你下载个几g的文件
+        if 'Content-Length' in r.headers.keys() and int(r.headers['Content-Length']) > 50000:
             print('[*]主机 ' + scan_url_port + ' 端口服务为：' + '大文件')
         else:
             r_detectencode = chardet.detect(r.content)
